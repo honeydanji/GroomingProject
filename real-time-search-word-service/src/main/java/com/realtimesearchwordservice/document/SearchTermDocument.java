1f428a5190729218cb603c5a6cd2851782b6b3ca
honeydanji <tjdwls3680@naver.com>
2023-12-11 16:58:37 +0900

    FEAT : ES 매핑할 검색쿼리에 대한 Document 생성

---
+++ real-time-search-word-service/src/main/java/com/realtimesearchwordservice/document/SearchTermDocument.java
@@ -0,0 +1,21 @@
+package com.realtimesearchwordservice.document;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "searchTerm")
+public class SearchTermDocument {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Getter
+    @Setter
+    @Field(type = FieldType.Text)
+    private String searchTerm;
+}
