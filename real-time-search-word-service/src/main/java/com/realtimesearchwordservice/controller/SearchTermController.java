9a7cad84a0d9185f48b797dae24f4b3f8bb1651f
honeydanji <tjdwls3680@naver.com>
2023-12-11 22:16:19 +0900

    FEAT : 검색쿼리 저장 controller 생성
    // 추가
    - Lombok 의 `@RequiredArgsConstructor`를 활용
    - 검색어 저장 로직을 수행하는 서비스 메소드 호출
    - 예외 발생 시 적절한 HTTP 응답 반환
    
    // issue number = 1

---
+++ real-time-search-word-service/src/main/java/com/realtimesearchwordservice/controller/SearchTermController.java
@@ -0,0 +1,26 @@
+package com.realtimesearchwordservice.controller;
+
+import com.realtimesearchwordservice.document.SearchTermDocument;
+import com.realtimesearchwordservice.service.SearchTermService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchTermController {
+
+    private final SearchTermService searchTermService;
+
+    @PostMapping("/document")
+    public ResponseEntity<String> saveDocumentController(@RequestBody SearchTermDocument searchTermDocument) {
+        try {
+            searchTermService.saveDocumentService(searchTermDocument);
+            return ResponseEntity.ok("검색어가 성공적으로 저장 되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("검색어 저장중 에러발생.");
+        }
+    }
+}
