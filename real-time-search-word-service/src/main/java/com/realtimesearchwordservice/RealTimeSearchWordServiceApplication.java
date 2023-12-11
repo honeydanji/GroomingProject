78cc2cdff51ed78992ba277ba55be7813aacdf4e
honeydanji <tjdwls3680@naver.com>
2023-12-11 15:53:16 +0900

    init

---
+++ real-time-search-word-service/src/main/java/com/realtimesearchwordservice/RealTimeSearchWordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.realtimesearchwordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeSearchWordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeSearchWordServiceApplication.class, args);
+	}
+
+}
---
+++ real-time-search-word-service/src/test/java/com/realtimesearchwordservice/RealTimeSearchWordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimesearchwordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeSearchWordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
