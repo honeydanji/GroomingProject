d8f6d9f0bf3c4eb32ef7366c05ebbd50537d3e7b
honeydanji <tjdwls3680@naver.com>
2023-12-14 17:45:31 +0900

    REFACTOR : Elasticsearch 기본 설정 변경

--- real-time-search-word-service/src/main/java/com/realtimesearchwordservice/config/ElasticSearchConfig.java
+++ real-time-search-word-service/src/main/java/com/realtimesearchwordservice/config/ElasticSearchConfig.java
@@ -1,17 +1,22 @@
 package com.realtimesearchwordservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories
+@EnableElasticsearchRepositories(basePackages = "com.realtimesearchwordservice.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    public String elasticSearchUris;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(elasticSearchUris)
                 .build();
     }
 }
