7d1c78ecec7b5296f3d5adb262b4b651e374f9e3
honeydanji <tjdwls3680@naver.com>
2024-01-22 12:15:07 +0900

    init

---
+++ album-service/src/test/java/com/albumservice/AlbumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
