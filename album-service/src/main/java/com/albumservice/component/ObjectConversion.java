4e3c25bd8ca4c49f868548c88a46222bf01ad1d5
honeydanji <tjdwls3680@naver.com>
2024-01-24 12:27:51 +0900

    feat(AlbumService) : 객체 변환 클래스 분리 및 생성

---
+++ album-service/src/main/java/com/albumservice/component/ObjectConversion.java
@@ -0,0 +1,23 @@
+package com.albumservice.component;
+
+import com.albumservice.dto.AlbumDto;
+import com.albumservice.entity.Album;
+import com.albumservice.vo.RequestAlbum;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectConversion {
+    public AlbumDto albumRequestToDto(RequestAlbum requestAlbum) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper.map(requestAlbum, AlbumDto.class);
+    }
+
+    public Album albumDtoToEntity(Album albumEntity) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper.map(albumEntity, Album.class);
+    }
+}
